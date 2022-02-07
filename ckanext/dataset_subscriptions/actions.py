459fe46f9dc0da6464fdc1c4752ec9379c45d762
Michal Ful <michal@fjelltopp.org>
2022-02-07 13:17:39 +0100

    Improve tests, fix unicode conversion to latin1, filter-out activities that are already sent

--- ckanext/dataset_subscriptions/actions.py
+++ ckanext/dataset_subscriptions/actions.py
@@ -1,9 +1,12 @@
 import ckan.plugins.toolkit as toolkit
 import ckan.logic as logic
+from ckan.lib.email_notifications import send_notification
 import ckan.lib.email_notifications as email_notifications
 import ckan.model as model
 import ckan.lib.base as base
 from ckan.common import config
+import unihandecode
+import datetime
 
 
 @toolkit.chained_action
@@ -15,33 +18,45 @@ def send_email_notifications(original_action, context, data_dict):
 
 
 def latin_username_send_notification(user, email_dict):
-   # fix for AWS SES not supporting UTF8 encoding of recepient field
-   # https://docs.aws.amazon.com/cli/latest/reference/ses/send-email.html
-   user['display_name'] = user['display_name'].encode('latin-1', 'replace')
-   return email_notifications.send_notification(user, email_dict)
+    # fix for AWS SES not supporting UTF8 encoding of recepient field
+    # https://docs.aws.amazon.com/cli/latest/reference/ses/send-email.html
+    user['display_name'] = unihandecode.unidecode(user['display_name'])
+    return send_notification(user, email_dict)
 
 
 def _add_dataset_name_to_activity_list(activity_list, context):
-        for index, activity in enumerate(activity_list):
-            object_id = activity['object_id']
-            dataset = toolkit.get_action('package_show')(
-                        context, {'id': object_id})
-            dataset_name = dataset['name']
-            activity_list[index]['dataset_name'] = dataset_name
-        return activity_list
+    for index, activity in enumerate(activity_list):
+        object_id = activity['object_id']
+        dataset = toolkit.get_action('package_show')(
+                    context, {'id': object_id})
+        dataset_name = dataset['name']
+        activity_list[index]['dataset_name'] = dataset_name
+    return activity_list
+
+
+def _filter_out_old_activites(activity_list, since):
+    strptime = datetime.datetime.strptime
+    fmt = '%Y-%m-%dT%H:%M:%S.%f'
+    activity_list = [activity for activity in activity_list
+                     if strptime(activity['timestamp'], fmt) > since]
+    return activity_list
+
 
 def dms_notification_provider(user_dict, since):
     context = {'model': model, 'session': model.Session,
                'user': user_dict['id']}
     activity_list = logic.get_action('dashboard_activity_list')(context, {})
-    # Return only activites that contain dataset changes
-    activity_list = [activity for activity in activity_list
-                     if activity['user_id'] != user_dict['id']
-                     and 'package' in activity['activity_type']]
+    dataset_activity_list = [activity for activity in activity_list
+                             if activity['user_id'] != user_dict['id']
+                             and 'package' in activity['activity_type']]
     # We want a notification per changed dataset, not a list of all changes
-    deduplicated_activity_list = list({item["object_id"]: item for item in activity_list}.values())
-    updated_activity_list = _add_dataset_name_to_activity_list(deduplicated_activity_list, context)
-    return dms_notifications_for_activities(updated_activity_list, user_dict)
+    timestamp_sorted_activity_list = sorted(dataset_activity_list,
+                                            key = lambda item: item['timestamp'])
+    deduplicated_activity_list = list({item["object_id"]:
+                                       item for item in timestamp_sorted_activity_list}.values())
+    activity_list_with_dataset_name = _add_dataset_name_to_activity_list(deduplicated_activity_list, context)
+    recent_activity_list = _filter_out_old_activites(activity_list_with_dataset_name, since)
+    return dms_notifications_for_activities(recent_activity_list, user_dict)
 
 
 def dms_notifications_for_activities(activities, user_dict):
--- ckanext/dataset_subscriptions/tests/test_actions.py
+++ ckanext/dataset_subscriptions/tests/test_actions.py
@@ -1,17 +1,18 @@
 from ckan.tests import factories
 import ckan.lib.email_notifications as email_notifications
+import ckanext.dataset_subscriptions.actions as actions
 import ckan.tests.helpers as helpers
 import pytest
 import datetime
-import mock
 
 
-@pytest.mark.ckan_config('ckan.plugins', 'dataset_subscriptions')
+@pytest.mark.ckan_config('ckan.plugins')
 @pytest.mark.usefixtures("with_plugins")
 @pytest.mark.usefixtures("clean_db")
 def create_user_with_resources(with_activity):
-    subscribed_user = factories.User(name='user5', activity_streams_email_notifications=True)
-    active_user = factories.User(name='user6')
+    subscribed_user = factories.User(name='user1',
+                                     activity_streams_email_notifications=True)
+    active_user = factories.User(name='user2')
     organization = factories.Organization(
         users=[
             {'name': subscribed_user['name'], 'capacity': 'member'},
@@ -25,8 +26,9 @@ def create_user_with_resources(with_activity):
             user=active_user
         )
         helpers.call_action(
-            "follow_dataset", context={"user": subscribed_user["name"]}, **dataset
-        )
+            "follow_dataset", context={"user": subscribed_user["name"]},
+            **dataset
+            )
         factories.Resource(
                  package_id=dataset['id'],
                  user=active_user
@@ -35,31 +37,48 @@ def create_user_with_resources(with_activity):
 
 
 @pytest.mark.usefixtures("clean_db")
+@pytest.mark.usefixtures("with_plugins")
 def test_with_no_dataset_updates():
-    user=create_user_with_resources(with_activity=False)
-    activities = [activity for activity in 
-                helpers.call_action("dashboard_activity_list", context={"user": user["id"]})
-                if "package" in activity["activity_type"]]
+    user = create_user_with_resources(with_activity=False)
+    activities = [activity for activity in
+                  helpers.call_action("dashboard_activity_list",
+                                      context={"user": user["id"]})
+                  if "package" in activity["activity_type"]]
     assert [activity["activity_type"] for activity in activities] == []
 
 
 @pytest.mark.usefixtures("clean_db")
+@pytest.mark.usefixtures("with_plugins")
 def test_with_dataset_updates():
-    user=create_user_with_resources(with_activity=True)
-    activity_list = helpers.call_action("dashboard_activity_list", context={"user": user["id"]})
+    user = create_user_with_resources(with_activity=True)
+    activity_list = helpers.call_action("dashboard_activity_list",
+                                        context={"user": user["id"]})
     # We'll be returning only package notifications
     filtered_activity_list = [activity for activity in activity_list
-                if "package" in activity["activity_type"]]
-    assert ["package" in activity["activity_type"] for activity in filtered_activity_list]
-    
+                              if "package" in activity["activity_type"]]
+    assert ["package" in activity["activity_type"]
+            for activity in filtered_activity_list]
 
 
-@pytest.mark.usefixtures("with_request_context", "clean_db")
+@pytest.mark.usefixtures("with_request_context")
+@pytest.mark.usefixtures("clean_db")
+@pytest.mark.usefixtures("with_plugins")
 @pytest.mark.ckan_config("ckan.activity_streams_email_notifications", True)
-@mock.patch("ckan.logic.action.update.request")
-def test_send_notification_injection(mock_request):
-    user=create_user_with_resources(with_activity=True)
-    notifications = email_notifications.get_notifications(user, datetime.datetime.fromtimestamp(3600))
-    # we expect two notifications - one for a new dataset and one for a resource
-    assert [ "2 new activities from CKAN" in notification["subject"]  for notification
-            in notifications ]
+def test_send_notification_injection():
+    user = create_user_with_resources(with_activity=True)
+    notifications = email_notifications.get_notifications(user,
+                                                          datetime.datetime.fromtimestamp(3600))
+    # we expect two notifications, one for a new dataset and one for a resource
+    assert ["2 new activities from CKAN" in notification["subject"]
+            for notification in notifications]
+
+
+@pytest.mark.usefixtures("with_request_context")
+@pytest.mark.usefixtures("clean_db")
+@pytest.mark.usefixtures("with_plugins")
+@pytest.mark.ckan_config("ckan.activity_streams_email_notifications", True)
+def test_dataset_subscriptions_notification_email_flow(mail_server):
+    create_user_with_resources(with_activity=True)
+    helpers.call_action("send_email_notifications")
+    msgs = mail_server.get_smtp_messages()
+    assert len(msgs) == 1
