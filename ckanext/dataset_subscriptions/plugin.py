aef882ecc16e319138bddbec52a376b92635ada1
Tomek Sabała <tomek@fjelltopp.org>
2022-03-23 13:39:57 +0100

    Phone number validation in user edit form. (#9)
    * Phone number validation in user edit form.
    
    * Fixup

--- ckanext/dataset_subscriptions/plugin.py
+++ ckanext/dataset_subscriptions/plugin.py
@@ -9,13 +9,12 @@ class DatasetSubscriptionsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IActions)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-                             'dataset_subscriptions')
+        toolkit.add_resource('assets', 'ckanext-dataset-subscriptions')
 
+    # IActions
     def get_actions(self):
         return {
             'send_email_notifications': actions.send_email_notifications,
